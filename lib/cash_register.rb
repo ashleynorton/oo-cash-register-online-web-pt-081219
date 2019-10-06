ae36a85c60a973ade970c72ca9a573450d514d1b
Ashley Norton <nortonashleym@gmail.com>
2019-10-06 21:33:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,8 @@
 class CashRegister
   attr_accessor :total, :discount, :title, :price, :quantity
   
+  
+  
   def initialize(total = 0, discount = 20)
     @total = total
     @discount = discount 
