a4f73b87f70540233f06b296e6034805d66131db
Ashley Norton <nortonashleym@gmail.com>
2019-10-06 22:26:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,7 +28,7 @@ class CashRegister
   end 
   
   def void_last_transaction
-    self.total = self.total - self.last_transaction.pop
-    self.total
+    self.total -= self.last_transaction.pop
+    
   end 
 end 
