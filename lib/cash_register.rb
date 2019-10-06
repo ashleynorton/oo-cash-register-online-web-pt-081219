d6cd4934277168c5056f45b2dd608376602e5d01
Ashley Norton <nortonashleym@gmail.com>
2019-10-06 22:07:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
-    self.items << title 
+    self.items.fill << title 
     self.last_transaction << self.total 
   end 
   
